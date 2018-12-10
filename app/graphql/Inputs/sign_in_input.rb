382f29bb5e798ae44b436b047a12c89218029383
Ivan E. Rodriguez <ivrodriguez@Ivans-MacBook-Pro-2.local>
2018-12-09 23:32:29 -0500

    - Removed unused files

--- app/graphql/Inputs/sign_in_input.rb
+++
@@ -1,7 +0,0 @@
-Inputs::SignInInput = GraphQL::InputObjectType.define do
-  name 'SignInInput'
-  description 'An input object representing arguments for a sign in call'
-
-  argument :email, types.String, "Email"
-  argument :password, types.String, "Password"
-end
